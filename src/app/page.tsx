cf85b2b9342266000ceaf4efcdd594e7b3b55fba
niklasfulle <fulle.niklas@web.de>
2023-08-03 20:42:02 +0200

    added some files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,46 @@
+import { Metadata } from "next";
+import LargeHeading from "@/components/ui/LargeHeading";
+import Paragraph from "@/components/ui/Paragraph";
+import Link from "next/link";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Wordldata API - Home",
+  description: "Free & Open Source API for Wordldata",
+};
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading size="lg" className="three-d text-black dark:text-light-gold">
+            Data of the world.
+          </LargeHeading>
+          <Paragraph className="max-w-xl lg:text-left">
+            Get the latest data of the world in JSON format. Data about countries, states, cities,
+            and more. All the data is free and open source. You can get the API key from{" "}
+            <Link
+              href="/login"
+              className="underline underline-offset-2 text-black dark:text-light-gold"
+            >
+              API Key
+            </Link>
+            .
+          </Paragraph>
+          <div className=" relative w-full max-w-lg lg:max-w-3xl lg:left-1/2 aspect-square lg:absolute">
+            <Image
+              priority
+              className="img-shadow"
+              quality={100}
+              style={{ objectFit: "contain" }}
+              fill
+              alt="Earth"
+              src="/earth.png"
+            ></Image>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

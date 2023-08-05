020fbd2363466fd41bf257fe1f4bee4c3fb94801
niklasfulle <fulle.niklas@web.de>
2023-08-06 00:10:25 +0200

    changed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import SignOutButton from "@/components/SignOutButton";
 
 const Navbar = async () => {
   const session = await getServerSession(authOptions);
-  
+
   return (
     <div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
       <div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
@@ -23,10 +23,10 @@ const Navbar = async () => {
         <div className="hidden md:flex gap-4">
           <ThemeToggle />
           <Link href="/documentation" className={buttonVariants({ variant: "ghost" })}>
-            Docs
+            Documentation
           </Link>
-          <Link href="/data" className={buttonVariants({ variant: "ghost" })}>
-            Data
+          <Link href="/world-data" className={buttonVariants({ variant: "ghost" })}>
+            World Data
           </Link>
           {session ? (
             <>
